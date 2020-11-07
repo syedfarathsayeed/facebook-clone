f46a9765e8615846c52181940c8ab7c39886f999
Syed Farath Sayeed <syedfarathsayeed@MacBook-Pro-de-Syed.local>
2020-11-07 14:22:06 +0100

    lets build facebook clone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Lets build facebook clone
+      </h1>
+    </div>
+  );
+}
+
+export default App;
